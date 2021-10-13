530e87bb70695d9b0074d51b04b9ae44271bede4
SuzannaO <ostrovskaya.s3@gmail.com>
2021-10-13 20:53:04 +0300

    NameS

--- Students/src/Names.java
+++ Students/src/Names.java
@@ -1,5 +1,10 @@
 public class Names {
     public static void main(String[] args) {
         System.out.println("Group 4");
+        Susan();
+    }
+    public static void Susan()
+    {
+        System.out.println("My name is Susan");
     }
 }
