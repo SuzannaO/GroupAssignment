991c187ab2efd2ce153d19c6ec558276eaed0ca5
mili81 <milisenta81@gmail.com>
2021-10-13 21:06:55 +0300

    initial commit

--- Students/src/Names.java
+++ Students/src/Names.java
@@ -1,5 +1,11 @@
 public class Names {
     public static void main(String[] args) {
         System.out.println("Group 4");
+        Inese();
+    }
+
+    private static void Inese() {
+        System.out.println("Inese");
     }
 }
+
